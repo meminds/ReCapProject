70f94281f17175233088089a64e1b73eeaffae8d
Muhammet Bas <190201113@uzem.education>
2022-04-02 14:50:55 +0300

    Lesssons, HomeWork 10.4

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            if (rental.ReturnDate > DateTime.Now)
+            {
+                return new ErrorResult("Rental nıt completed...");
+            }
+            else
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult("Rental completed...");
+            }
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(), "Rental Listed..");
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(item => item.Id == rentalId));
+        }
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,8 +11,61 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarTest();
+            //CarTest();
             //BrandTest();
+            //UserAddTest();
+            //UserDeleteTest();
+            //CustomerAddTest();
+            //RantalControllerTest();
+        }
+
+        private static void RantalControllerTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            rentalManager.Add(new Rental { CarId = 3, CustomerId = 1, RentDate = new DateTime(2022, 03, 18, 09, 00, 00), ReturnDate = new DateTime(2022, 03, 19, 09, 00, 00) });
+            foreach (var item in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine(item.CustomerId);
+            }
+        }
+
+        private static void CustomerAddTest()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            customerManager.Add(new Customer { UserId = 10, CompanyName = "Inseder" });
+        }
+
+        private static void UserDeleteTest()
+        {
+            User user = new User();
+            user.UserId = 16;
+            UserManager userManager = new UserManager(new EfUserDal());
+            userManager.Delete(user);
+            foreach (var item in userManager.GetAll().Data)
+            {
+                Console.WriteLine(item.UserId + " / " + item.FirstName);
+            }
+        }
+
+        private static void UserAddTest()
+        {
+            /*
+            User user = new User() { FirstName = "Fuat", LastName = "Demiroğ", Email = "assd@mail", Password = "34343" };
+            User user1 = new User() { FirstName = "Derya", LastName = "Asa", Email = "fgdfg@mail", Password = "12343" };
+            User user2 = new User() { FirstName = "Naz", LastName = "İlhan", Email = "cvxcvxc@mail", Password = "12343" };
+            User user3 = new User() { FirstName = "Sevinç", LastName = "Sahin", Email = "ytytyty@mail", Password = "143" };
+            User user4 = new User() { FirstName = "Hikmet", LastName = "Alemdar", Email = "nmnmnm@mail", Password = "12334" };
+            */
+            UserManager userManager = new UserManager(new EfUserDal());
+            /*userManager.Add(user);
+            userManager.Add(user1);
+            userManager.Add(user2);
+            userManager.Add(user3);
+            userManager.Add(user4);*/
+            foreach (var item in userManager.GetAll().Data)
+            {
+                Console.WriteLine(item.UserId + " / " + item.FirstName);
+            }
         }
 
         private static void BrandTest()
